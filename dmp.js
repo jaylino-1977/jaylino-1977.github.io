3aad035b23514d3138c1f481ee5e2c4da8f05d61
jaylino-1977 <54541679+jaylino-1977@users.noreply.github.com>
2023-01-26 17:26:00 -0600

    Add files via upload

---
+++ dmp.js
@@ -0,0 +1,26 @@
+﻿
+// ******************************* Module 2 Assignment ******************************* //
+var graphics = 1000;
+graphics = graphics + 1000
+
+
+var calligraphy = "font design"; //This is a global variable
+var photography = "head shot"; //This is a global variable
+var videography = "commercial production" //This is a global variable
+
+{ 
+   var graphics = 1000; //This is a block variable
+}
+
+let c = 1000 + 2000;
+let d = 3000 * 2;
+let e = 4000 - 500;
+let f = 6000 / 3000;
+
+let g = "juan" + " " + "Perez";
+
+const camera = {type:"Mirrorless", model:"CanonR6", color:"black"};
+
+
+
+console.log('');
