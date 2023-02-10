159d6126a3e406f718c0a6a36a76e4fd3bb58d00
jaylino-1977 <54541679+jaylino-1977@users.noreply.github.com>
2023-02-09 22:12:08 -0600

    module 4

--- dmp.js
+++ dmp.js
@@ -24,26 +24,52 @@
 // ****************************** Module 3 Assignment ******************************** //
 
 //functions//
-function gallery (fonts, graphics, logos) {
-   return fonts + graphics + logos; 
-}
-function gallery2 (fonts1, graphics1, logos1) {
-  return fonts1 + graphics1 + logos1; 
-}
+//function gallery (fonts, graphics, logos) {
+   //return fonts + graphics + logos; 
+//}
+//function gallery2 (fonts1, graphics1, logos1) {
+  //return fonts1 + graphics1 + logos1; 
+//}
 //objects with properties//
-const lettering = {typeface: "Arial", weight: "16px", color:"black"}
-const graphic = {type: "raster" }
-const logo = {type: "vector"}
+////const lettering = {typeface: "Arial", weight: "16px", color:"black"}
+//const graphic = {type: "raster" }
+//const logo = {type: "vector"}
 
 //Event//
 //Check index.html for event - font change in size
 
 //method//
-const font = {
-  fontName: "Arial",
-  fontweight : "16",
-  color     : "black",
-  fontFamily : function() {
-    return this.fontName + this.fontweight + this.color;
-  }
-};
+//onst font = {
+  //fontName: "Arial",
+  //fontweight : "16",
+  //color     : "black",
+  //fontFamily : function() {
+    //return this.fontName + this.fontweight + this.color;
+  //}
+//};
+//******************************** Module 4 Assigment ***********************************/
+const myGraphics = 90;
+const myLogos = 299;
+const myPhotography = 75;
+const myVideography = 120;
+const myCalligraphy = 199;
+const myIllustration = 80;
+
+const job = "graphics";
+const job2 = "logo";
+const job3 = "photography";
+const job4 = "videography";
+const job5 = "calligraphy";
+const job6 = "illustration";
+
+const workFee = myGraphics + job;
+console.log(`I need a quote for ${job2}, ${job3} and ${job5}`);
+console.log(workFee);
+
+if (myGraphics < 45) {
+  console.log("You need to pick 1 more option!");
+}  else {
+  console.log("You are ready for a graphics consultation!");
+}
+
+ 
