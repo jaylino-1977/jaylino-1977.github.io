c4d970423116013542506458cad864c38c029aa8
jaylino-1977 <54541679+jaylino-1977@users.noreply.github.com>
2023-05-09 11:07:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,136 +1,62 @@
-const closedFace = document.querySelector('.closed');
-const openFace = document.querySelector('.open');
+//*************************** buttons *******************************/
+ 
+ const button = document.querySelector('.button');
+ const button2 = document.querySelector('.button2');
+ const button3 = document.querySelector('.button3');
+ 
+ button.addEventListener('mouseenter', () => {
+   if (!button.classList.contains('hover')) {
+    button.classList.add('hover');
+   }
+ });
+ 
+ button.addEventListener('mouseleave', () => {
+   if (button.classList.contains('hover')) {
+     button.classList.remove('hover');
+   }
+ });
+ 
+ button2.addEventListener('mouseenter', () => {
+     if (!button2.classList.contains('hover')) {
+        button2.classList.add('hover');
+     }
+   });
+   
+ button2.addEventListener('mouseleave', () => {
+     if (button2.classList.contains('hover')) {
+        button2.classList.remove('hover');
+     }
+   });
+ 
+ button3.addEventListener('mouseenter', () => {
+   if (!button3.classList.contains('hover')) {
+    button3.classList.add('hover');
+   }
+ });
+ 
+ button3.addEventListener('mouseleave', () => {
+   if (button3.classList.contains('hover')) {
+    button3.classList.remove('hover');
+   }
+ });
+ //*************************************************************** */
 
-const closedFace2 = document.querySelector('.closed2');
-const openFace2 = document.querySelector('.open2');
-
-const closedFace3 = document.querySelector('.closed3');
-const openFace3 = document.querySelector('.open3');
-
-// one
-closedFace.addEventListener('click', () => {
-  if (openFace.classList.contains('open')) {
-    openFace.classList.add('active');
-    closedFace.classList.remove('active');
-  }
+ var post= document.getElementById("post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
 });
-
-openFace.addEventListener('click', () => {
-  if (closedFace.classList.contains('closed')) {
-    closedFace.classList.add('active');
-    openFace.classList.remove('active');
-  }
-});
-
-// two
-closedFace2.addEventListener('click', () => {
-  if (openFace2.classList.contains('open2')) {
-    openFace2.classList.add('active2');
-    closedFace2.classList.remove('active2');
-  }
-});
-
-openFace2.addEventListener('click', () => {
-  if (closedFace2.classList.contains('closed2')) {
-    closedFace2.classList.add('active2');
-    openFace2.classList.remove('active2');
-  }
-});
-
-// three
-closedFace3.addEventListener('click', () => {
-  if (openFace3.classList.contains('open3')) {
-    openFace3.classList.add('active3');
-    closedFace3.classList.remove('active3');
-  }
-});
-
-openFace3.addEventListener('click', () => {
-  if (closedFace3.classList.contains('closed3')) {
-    closedFace3.classList.add('active3');
-    openFace3.classList.remove('active3');
-  }
-});
-//**************************************************************************/
-let data = [
-    {
-      name: 'Juan',
-      age: '45',
-      active: '21'
-    },
-    {
-      name: 'Tanya',
-      age: '45',
-      active: '21'
-    },
-    {
-      name: 'Hector',
-      age: '55',
-      active: '19'
-    },
-    {
-      name: 'Paul',
-      age: '53',
-      active: '15'
-    },
-    {
-      name: 'David',
-      age: '60',
-      active: '10'
-    },
-    {
-      name: 'Ray',
-      age: '52',
-      active: '5'
+//*************************************************************** */
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("sample").innerHTML = this.responseText;
     }
-  ];
-  
-  const info = document.querySelector('#info');
-  
-  let details = data.map(function(item) {
-    return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' +  ' and has been active for ' + item.active + ' years' + '</div>';
-  });
-  
-  info.innerHTML = details.join('\n');
-
-  //*********************************************************************/
-
-  const circle = document.querySelector('#circle');
-const circle2 = document.querySelector('#circle2');
-const circle3 = document.querySelector('#circle3');
-
-circle.addEventListener('mouseenter', () => {
-  if (!circle.classList.contains('hover')) {
-    circle.classList.add('hover');
-  }
-});
-
-circle.addEventListener('mouseleave', () => {
-  if (circle.classList.contains('hover')) {
-    circle.classList.remove('hover');
-  }
-});
-
-circle2.addEventListener('mouseenter', () => {
-    if (!circle2.classList.contains('hover')) {
-      circle2.classList.add('hover');
-    }
-  });
-  
-  circle2.addEventListener('mouseleave', () => {
-    if (circle2.classList.contains('hover')) {
-      circle2.classList.remove('hover');
-    }
-  });
-
-circle3.addEventListener('mouseenter', () => {
-  if (!circle3.classList.contains('hover')) {
-    circle3.classList.add('hover');
-  }
-});
-
-circle3.addEventListener('mouseleave', () => {
-  if (circle3.classList.contains('hover')) {
-    circle3.classList.remove('hover');
-  }
-});
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
